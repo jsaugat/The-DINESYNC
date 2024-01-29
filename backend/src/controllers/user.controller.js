7889664f8df0c35a515aa2c6d882dfedee96f3bb
Mr. Saugat <jsaugatt02@gmail.com>
2024-01-29 23:18:11 +0545

    create token and send it as response to the client.

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,17 +1,31 @@
-//import mongoose from 'mongoose'; // to interact with user's collection in db and get or create new documents
-import { User } from "../models/user.model.js";
+import { User } from "../models/user.model.js"; // for User.signup() static method
+import jwt from "jsonwebtoken";
 
-//? login user
+// param _id is gonna be part of the payload of the token.
+const createToken = (_id) => {
+  // jwt.sign(payload, secret, option)
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
+
+//? LOGIN user
 const loginUser = async (req, res) => {
-  // async because, we'll communicate with db, here
+  // async because, we'll communicate with db here
   res.json(req.method);
 };
 
-//? signup user
+//? SIGNUP user
 const signupUser = async (req, res) => {
+  // retrieve email and pwd from the client request body
   const { email, password } = req.body;
-  User.signup(email, password) // 'backend\src\models\user.model.js'
-    .then((user) => res.status(200).json({ email, user }))
+  // pass the retrieved email and pwd to the signup validator to 'backend\src\models\user.model.js'
+  const user = User.signup(email, password); 
+  //! client req > validation > return { email, hashedPassword } as 'user'
+  user
+    .then((user) => {
+      // create a token
+      const token = createToken(user._id);
+      res.status(200).json({ email, token });
+    })
     .catch((error) => res.status(400).json({ error: error.message }));
 };
 
