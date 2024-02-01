44eb72d8bf695dbfd943ea72ae803cc8a3f0f66f
Mr. Saugat <jsaugatt02@gmail.com>
2024-02-01 18:29:25 +0545

    conditionally rendering login-signup or logout buttons

--- client/src/myComponents/Header/NavBar.jsx
+++ client/src/myComponents/Header/NavBar.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { Logo } from "/src/master";
 import { Button } from "@/components/ui/button"
 import { useLogout } from "@/hooks/useLogout.js";
+import { useAuthContext } from "@/hooks/useAuthContext.js";
 
 const NavBar = () => {
+  const [isLoggedIn, setIsLoggedIn ] = useState();
+
   const { logout } = useLogout();
+  const { user } = useAuthContext();
 
   return (
     <nav className="fixed z-50 w-full top-0 nav-menu flex px-[5rem] justify-between items-center py-[1.5rem] border-b border-onyx backdrop-blur-md font-montreal"> 
@@ -20,14 +24,28 @@ const NavBar = () => {
         <li ><NavLink to="booking">Reservation</NavLink></li>
       </ul>
       <section>
-        <div>
-          <Button variant="login" onClick={()=> logout()}>Logout</Button>
-        </div>
+
+        {/* //? when is user logged in */}
+        {user && (
+          <div>
+            {/* // user.email is VALID when user is logged in
+                // user.email is NULL when user is not logged in 
+                //! you will see error if you render user.email when it is null :)
+            */}
+            <span>{user.email}</span>
+            <Button variant="login" onClick={()=> logout()}>Logout</Button>
+          </div>
+        )}
+
+        {/* //! when is user not logged in */}
+        {!user && (
+          <div className="space-x-3">
+            <NavLink to={"login"}><Button variant="login">Login</Button></NavLink>
+            <NavLink to={"signup"}><Button variant="secondary">Signup</Button></NavLink>
+          </div>)
+        }
       </section>
-      <div className="space-x-3">
-        <NavLink to={"login"}><Button variant="login">Login</Button></NavLink>
-        <NavLink to={"signup"}><Button variant="secondary">Signup</Button></NavLink>
-      </div>
+      
     </nav>
   );
 };
