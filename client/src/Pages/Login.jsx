21fd0f58265a8dedc1b409bd3ce06bb2a1e3c41b
Mr. Saugat <jsaugatt02@gmail.com>
2024-02-01 17:51:42 +0545

    login functionality

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,21 +1,23 @@
 import { useState } from "react";
 import { Container } from "../master";
 import { Button } from "@/components/ui/button";
+import { useLogin } from "@/hooks/useLogin.js";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { login, error, isLoading } = useLogin();
 
-  const handleSubmit = async (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
-    console.log(email, password);
+    await login(email, password);
   };
 
   const inputCSS = "rounded-lg px-4 py-2 bg-transparent border border-onyx/70 focus:ring-white/20 focus:border-white/20 w-[350px]"
 
   return (
     <Container>
-    <form className="h-[74vh] flex flex-col justify-around border border-onyx/0 rounded-[30px] w-fit px-24 py-24 mx-auto" onSubmit={handleSubmit}>
+    <form className="h-[74vh] flex flex-col justify-around border border-onyx/0 rounded-[30px] w-fit px-24 py-24 mx-auto" onSubmit={handleLogin}>
       <h3 className="text-6xl font-semibold">Log in</h3>
       <div className="flex flex-col items-start">
       <label htmlFor="" className="mb-2">Email</label>
@@ -34,8 +36,15 @@ function Login() {
         placeholder="Password"
         className={`${inputCSS}`}
       />
-      <Button variant="secondary" className="mt-16 w-full">Login</Button>
+      <Button 
+        disabled={isLoading}
+        variant="secondary" 
+        className="mt-16 w-full"
+      >
+        Login
+      </Button>
       </div>
+      {error && <div className="error">{error}</div>}
     </form>
     </Container>
   );
