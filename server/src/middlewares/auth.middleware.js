db5b05c4f561ba759c1fd5a7f0f67cedf064a9a9
Mr. Saugat <jsaugatt02@gmail.com>
2024-02-22 23:29:49 +0545

    build a shield ; auth middlware to verify jwt

---
+++ server/src/middlewares/auth.middleware.js
@@ -0,0 +1,29 @@
+import asyncHandler from "express-async-handler";
+
+export const shield = asyncHandler(async function (req, res, next) {
+  let token = req.cookies.jwt;
+  // if missing token
+  if (!token) {
+    handleUnauthorized(res, "Missing: token");
+  } else {
+    try {
+      /**
+       * @function 'decoded' - 
+       * if token is valid   @returns an object representing the 'decoded payload of the JWT'
+       * if token is invalid @throws  an error that's why wrapped in try-catch block
+       */
+      const decoded = jwt.verify(token, process.env.JWT_SECRET); // returns {_id}
+      // retrieve user-info from db based on decoded._id and assign it to a new 'user' prop in req obj.
+      // req.user can be utilized by downstream middlwares or route handlers.
+      req.user = await User.findById(decoded._id).select("-password");
+      next();
+    } catch (error) {
+      handleUnauthorized(res, "Invalid token");
+    }
+  }
+});
+
+const handleUnauthorized = (res, message) => {
+  res.status(401);
+  throw new Error(`Unauthorized: ${message}`);
+};
--- server/src/middlewares/error.middleware.js
+++ server/src/middlewares/error.middleware.js
@@ -1,11 +1,11 @@
-// if a request reaches this middlware, it means no route has been matched for the requested URL
+//? if a request reaches this middlware, it means no route has been matched for the requested URL
 const notFound = (req, res, next) => {
   const error = new Error(`Not Found - ${req.originalUrl}`);
   res.status(404);
   next(error);
 };
 
-// if any middleware or route handler before them calls the next(error), the control will be passed directly to the 'errorHandler' middleware.
+//? if any middleware or route handler before them calls the next(error), the control will be passed directly to the 'errorHandler' middleware.
 const errorHandler = (err, req, res, next) => {
   let message = err.message;
   let statusCode = res.statusCode !== 200 ? res.statusCode : 500;
@@ -16,8 +16,8 @@ const errorHandler = (err, req, res, next) => {
     message = "Resource not found";
   }
   res.status(statusCode).json({
-    message,
     status_code: statusCode,
+    message,
     stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
 };
