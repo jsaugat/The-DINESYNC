0a465790aaf605c455ebb4f2173ec312a4ed225b
Mr. Saugat <jsaugatt02@gmail.com>
2024-02-23 23:24:17 +0545

    import and add authSlice to the store

--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./slices/authSlice"; 
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: authReducer,
+  },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware(), // uses the default middleware provided by Redux Toolkit for store.
   devTools: true, // Enables the use of Redux DevTools Extension.
 });
