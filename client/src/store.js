6fc654e2b1da4f2d0d13290dc1b216b78e14da79
Mr. Saugat <jsaugatt02@gmail.com>
2024-02-24 12:54:38 +0545

    import apiSlice and use it in reducer as [apiSlice.reducerPath]: apiSlice.reducer

--- client/src/store.js
+++ client/src/store.js
@@ -1,11 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./slices/authSlice"; 
+import authReducer from "./slices/authSlice";
+import { apiSlice } from "./slices/apiSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer, // api state will be displayed in the redux devtools - https://shorturl.at/eDKZ0
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware(), // uses the default middleware provided by Redux Toolkit for store.
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware), // uses the default middleware provided by Redux Toolkit for store.
   devTools: true, // Enables the use of Redux DevTools Extension.
 });
 
