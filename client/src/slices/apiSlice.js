cd71058b2f9629500f016296eb098ac8de1d39c1
Mr. Saugat <jsaugatt02@gmail.com>
2024-02-24 12:59:53 +0545

    create usersApiSlice and inject login endpoints to mutate data

---
+++ client/src/slices/apiSlice.js
@@ -0,0 +1,19 @@
+/**
+ * @apiSlice : parent to any other api slices like usersApiSlice, productsApiSlice, etc.
+    - requests won't be from this file
+    -   
+    - to handle async requests
+ * Implements Thunk Middleware with abstraction?
+ */
+
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const baseQuery = fetchBaseQuery({ baseUrl: "" }); // it's empty cause we used proxy in vite.config.js
+
+export const apiSlice = createApi({
+  baseQuery,
+  tagTypes: ["User"], // caching
+  endpoints: (builder) => ({}),
+  // builder = an object that allows you to define various aspects of your API like queries, mutations etc.
+  // all other api slices will have their own endpoints
+});
---
+++ client/src/slices/usersApiSlice.js
@@ -0,0 +1,18 @@
+import { apiSlice } from "./apiSlice"; // import parent
+import { USERS_URL } from "@/constants";
+
+export const usersApiSlice = apiSlice.injectEndpoints({ // inject endpoints to the apiSlice parent
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({ // data is going to be email, pw etc.
+        url: `${USERS_URL}/login`,
+        method: "POST",
+        body: data,
+      }) 
+    }),
+  }),
+})
+
+export const {
+  useLoginMutation
+} = usersApiSlice;
