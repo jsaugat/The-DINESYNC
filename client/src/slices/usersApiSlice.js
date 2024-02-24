d3f3df99049d90b39393e31e8440f839f5016a47
Mr. Saugat <jsaugatt02@gmail.com>
2024-02-24 22:10:12 +0545

    add updateProfile userApiSlice

--- client/src/slices/usersApiSlice.js
+++ client/src/slices/usersApiSlice.js
@@ -1,34 +1,41 @@
-import { apiSlice } from "./apiSlice"; // the Parent of slices 
+import { apiSlice } from "./apiSlice"; // the Parent of slices
 import { USERS_URL } from "@/constants";
 
-export const usersApiSlice = apiSlice.injectEndpoints({ // inject endpoints to the apiSlice parent
+export const usersApiSlice = apiSlice.injectEndpoints({
+  // inject endpoints to the apiSlice parent
   endpoints: (builder) => ({
     login: builder.mutation({
-      query: (data) => ({ // data is going to be email, pw etc.
+      query: (data) => ({
+        // data is going to be email, pw etc.
         url: `${USERS_URL}`,
         method: "POST",
         body: data,
-      }) 
+      }),
     }),
     register: builder.mutation({
-      query: (data) => ({ // data is going to be email, pw etc.
+      query: (data) => ({
+        // data is going to be email, pw etc.
         url: `${USERS_URL}/register`,
         method: "POST",
         body: data,
-      }) 
+      }),
     }),
     logout: builder.mutation({
       query: () => ({
         url: `${USERS_URL}/logout`,
         method: "POST",
-      })
+      }),
     }),
-    update
+    updateProfile: builder.mutation({
+      query: (data) => ({
+        // data is going to be email, pw etc.
+        url: `${USERS_URL}/profile`,
+        method: "PUT",
+        body: data,
+      }),
+    }),
   }),
-})
+});
 
-export const {
-  useLoginMutation,
-  useLogoutMutation,
-  useRegisterMutation
-} = usersApiSlice;
+export const { useLoginMutation, useLogoutMutation, useRegisterMutation, useUpdateProfileMutation } =
+  usersApiSlice;
