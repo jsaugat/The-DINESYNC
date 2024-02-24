168b6275ff8aa5965494d4da04e0c63b51262b3a
Mr. Saugat <jsaugatt02@gmail.com>
2024-02-24 16:36:12 +0545

    customize global midleware

--- server/src/index.js
+++ server/src/index.js
@@ -3,42 +3,37 @@ import dotenv from "dotenv";
 import connectDB from "./database/connection.js";
 // import usersRoute from "./routes/users.js";
 import authRoute from "./routes/auth.js";
-import { notFoundHandler, errorHandler } from "./middlewares/error.middleware.js";
+import {
+  notFoundHandler,
+  errorHandler,
+} from "./middlewares/error.middleware.js";
 // import tablesRoute from "./routes/tables.js";
 
 dotenv.config(); // can use process.env now
 const port = process.env.PORT || 6900;
 
-// GLOBAL middleware
+//? GLOBAL middleware
 app.use("/", (req, res, next) => {
-  console.log("Request Method -> " + req.method);
-  console.log("Request Path -> " + req.url);
-  console.log("Request Body -> " + JSON.stringify(req.body));
+  console.log("\x1b[32m%s\x1b[0m", "REQUEST DETAILS :");
+  console.table({
+    time: new Date().toLocaleTimeString(),
+    method: req.method,
+    path: req.url,
+    // body: JSON.stringify(req.body)
+  });
+  console.log("\x1b[32m%s\x1b[0m", "REQUEST BODY :");
+  console.table(req.body);
   next();
 });
 
-// ROUTES middlewares
+//? ROUTES middlewares
 app.use("/api/auth", authRoute);
 // app.use("/api/tables", tablesRoute);
 // app.use("/api/users", usersRoute);
 
-// 404 and error handler middlewares
+//? 404 and error handler middlewares
 app.use(notFoundHandler);
 app.use(errorHandler);
-
-// ERROR Handler middleware : triggered if there's an error passed to next() in any previous middleware or route handler.
-// app.use((error, req, res, next) => {
-//   const message = error.message || "Something went wrong";
-//   const status = error.status;
-
-//   return res.status(status).json({
-//     success: false,
-//     status,
-//     message,
-//     stack: error.stack,
-//   });
-// });
-//
 
 //? Database connection : async function returns a Promise
 connectDB()
