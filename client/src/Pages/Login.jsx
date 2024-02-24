80375aa363448c9bf68d8406cbe348498d91f455
Mr. Saugat <jsaugatt02@gmail.com>
2024-02-24 13:34:56 +0545

    configure slices and state in login file.

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,20 +1,43 @@
 import { useEffect, useRef, useState } from "react";
 import { Container } from "../master";
 import { Button } from "@/shadcn/ui/button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux"; // to get user data from state and also dispatch
+import { useLoginMutation } from "@/slices/usersApiSlice"; // hit backend api
+import { setCredentials } from "@/slices/authSlice"; // after hitting backend api and getting data we gotta set it to STATE and LOCAL-STORAGE
+import { toast } from "react-toastify";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const ref = useRef(null);
+  useEffect(() => {
+    ref.current.focus();
+  }, []);
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [login, { isLoading }] = useLoginMutation();
+
+  const { userInfo } = useSelector((state) => state.auth); // check auth state in devtools (https://shorturl.at/eDKZ0)
 
   useEffect(() => {
-    ref.current.focus()
-  }, [])
-  
+    if (userInfo) navigate("/");
+  }, [userInfo]);
 
   const loginHandler = async (e) => {
     e.preventDefault();
+    try {
+      //? Step1: hit backend api and retrieve data
+      const res = await login({ email, password }).unwrap(); // unwrap unwraps the promise?
+      //? Step2: dispatch
+      dispatch(setCredentials({ ...res }));
+      navigate("/");
+    } catch (err) {
+      console.log(err?.data?.message || err.error);
+      toast.error(err?.data?.message || err.error)
+    }
   };
 
   const inputCSS =
@@ -58,7 +81,10 @@ function Login() {
           </Button>
         </div>
         <div className="mt-12 text-[1.55rem]">
-           New Customer ? <Link to="/signup" className="text-blue-500 hover:underline">Create an account</Link>
+          New Customer ?{" "}
+          <Link to="/signup" className="text-blue-500 hover:underline">
+            Create an account
+          </Link>
         </div>
         {/* {error && <div className="error">{error}</div>} */}
       </form>
